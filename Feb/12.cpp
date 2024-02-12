579e4c69dbcfb69f40d2819991d9a605ec5f5c3e
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-02-12 18:59:09 +0530

    Create 12.cpp

---
+++ Feb/12.cpp
@@ -0,0 +1,27 @@
+/*
+Author : Ashish Kumar
+Date : 12/02/2024
+Problem : Majority Element
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/majority-element/description/
+*/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> f;
+
+        for(auto i : nums){
+            ++f[i];
+        }
+
+        for(auto i : f){
+            if(i.second > nums.size() / 2){
+                return i.first;
+            }
+        }
+
+        return 0;
+    }
+};f
