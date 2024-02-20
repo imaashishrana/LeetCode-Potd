161ea590385e3728411e4b89dbfa0c044f6dcade
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-02-20 17:22:59 +0530

    Create 20.cpp

---
+++ Feb/20.cpp
@@ -0,0 +1,23 @@
+/*
+
+Author : Ashish Kumar
+Date : 20/02/2024
+Problem : Missing Number
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/missing-number/description/?envType=daily-question&envId=2024-02-20
+
+*/
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = nums.size();
+        sum = (sum * (sum + 1)) / 2;
+
+        for(auto i : nums)
+            sum -= i;
+
+        return sum;
+    }
+};
