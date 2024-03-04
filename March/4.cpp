7f0fcbf52507cd03a81f31b56f4fbff57a188518
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-03-04 12:56:16 +0530

    Create 4.cpp

---
+++ March/4.cpp
@@ -0,0 +1,36 @@
+/*
+Author : Ashish Kumar
+Date : 04/03/2024
+Problem : Bag of Tokens
+Difficulty : Medium 
+Problem Link : https://leetcode.com/problems/bag-of-tokens/description/
+*/
+
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n = tokens.size();
+        int i = 0;
+        int j = n - 1;
+
+        int sc = 0;
+        sort(tokens.begin(), tokens.end());
+
+        while(i <= j) {
+            if(tokens[i] <= power) {
+                power -= tokens[i++];
+                ++sc;
+            }
+            else if(sc > 0 and i < j) {
+                power += tokens[j--];
+                --sc;
+            }
+            else{
+                break;
+            }
+        }
+
+        return sc;
+    }
+};
